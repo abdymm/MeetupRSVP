b424b27478ca65da321ea4ddbf6cf6f93fb0787c
“abdymm” <abdymalikmulky@gmail.com>
2022-11-18 23:57:52 +0700

    feat: added backhandler when user editing form

--- src/features/Registration/screens/index.tsx
+++ src/features/Registration/screens/index.tsx
@@ -1,6 +1,6 @@
 import {DatepicketInput, RadioInput} from '@Components';
-import React, {useState} from 'react';
-import {ScrollView, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Alert, BackHandler, ScrollView, View} from 'react-native';
 import {Button, TextInput} from 'react-native-paper';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
@@ -24,6 +24,38 @@ const INITIAL_FORM = {
 
 const RegistrationScreen = () => {
   const [form, setForm] = useState<User>(INITIAL_FORM);
+
+  useEffect(() => {
+    const formValues = Object.values(form);
+    const isEditing = formValues.find(
+      value => value !== '' || value !== null || value,
+    );
+
+    const backAction = () => {
+      if (isEditing) {
+        Alert.alert('Exit App', 'All of your inputs will be lost', [
+          {
+            text: 'Cancel',
+            style: 'cancel',
+          },
+          {
+            text: 'Yes',
+            onPress: () => BackHandler.exitApp(),
+          },
+        ]);
+        return true;
+      } else {
+        BackHandler.exitApp();
+      }
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction,
+    );
+
+    return () => backHandler.remove();
+  }, [form]);
 
   const onChange = (key: string, value: string | number) => {
     setForm({...form, [key]: value});
