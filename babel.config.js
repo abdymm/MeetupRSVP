f749fc662f44f70895d690eb4ea0c0d747447706
“abdymm” <abdymalikmulky@gmail.com>
2022-11-18 10:00:05 +0700

    build: added necessary dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,39 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  env: {
+    production: {
+      plugins: ['react-native-paper/babel'],
+    },
+  },
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          // Common
+          '@Constants': './src/common/constants',
+          '@Components': './src/common/components',
+          '@Services': './src/common/services',
+          '@Hooks': './src/common/hooks',
+          '@Utils': './src/common/utils',
+
+          // Features
+          '@Registration': './src/features/Registration',
+          '@User': './src/features/User',
+          '@Report': './src/features/Report',
+        },
+      },
+    ],
+  ],
 };
