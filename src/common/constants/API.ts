2fb7b5fad6bca93af822b8251fa107ad6f6c465b
“abdymm” <abdymalikmulky@gmail.com>
2022-11-20 12:42:28 +0700

    fix(API): change api mockup provider

--- src/common/constants/API.ts
+++ src/common/constants/API.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://meetup.free.beeceptor.com';
+export const BASE_URL = 'https://637990367eb4705cf2818865.mockapi.io';
 export const ENDPOINT_REGISTER = '/register';
 export const ENDPOINT_USERS = '/users';
